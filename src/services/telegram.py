5ba81672f466433bcf0caa002f9b064e55d954e6
Vladislav Bodrevskiy <anpathfinder@mail.ru>
2024-12-23 11:27:01 +0300

    add: telegram-service

---
+++ src/services/telegram.py
@@ -0,0 +1,147 @@
+import uuid
+import asyncio
+from time import time
+from datetime import datetime, timedelta
+
+from pyrogram import Client
+from pyrogram.types import Message
+from pyrogram.enums import MessageMediaType
+
+from src.utils.logger import logger
+from src.core.config import settings
+from src.services.repository import FileRepository
+
+
+class PyrogramClient:
+    def __init__(self, client: Client, repository: FileRepository):
+        self.app = client
+        self.repository = repository
+
+        self.semaphore = asyncio.Semaphore(5)
+
+        self.map = {}
+
+    def run(self):
+        try:
+            self.app.run(self.download_media())
+            logger.info("Хорошего собрания!")
+        except Exception as e:
+            logger.error(f"Ошибка при загрузке медиаконтента: {e}")
+
+    async def download_media(self):
+        logger.info("Начинаем загрузку медиаконтента...")
+
+        async with self.app:
+            app = self.app
+            start = time()
+
+            messages = app.get_chat_history(
+                chat_id=settings.TARGET,
+                limit=settings.LIMIT
+            )
+
+            tasks = []
+            async for message in messages:
+                if message.date < (datetime.today() - timedelta(days=settings.DAYS)):
+                    break
+
+                match message.media:
+                    case MessageMediaType.VIDEO:
+                        filepath = await self.process_message(
+                            MessageMediaType.VIDEO,
+                            message.date,
+                            message.video.file_name
+                        )
+
+                        task = asyncio.create_task(
+                            self.semaphore_wrapper(self._download_with_notification, message, filepath)
+                        )
+                        tasks.append(task)
+
+                    case MessageMediaType.DOCUMENT:
+                        filepath = await self.process_message(
+                            MessageMediaType.DOCUMENT,
+                            message.date,
+                            message.document.file_name
+                        )
+
+                        task = asyncio.create_task(
+                            self.semaphore_wrapper(self._download_with_notification, message, filepath)
+                        )
+                        tasks.append(task)
+
+                    case MessageMediaType.PHOTO:
+                        filepath = await self.process_message(
+                            MessageMediaType.PHOTO,
+                            message.date
+                        )
+
+                        task = asyncio.create_task(
+                            self.semaphore_wrapper(self._download_with_notification, message, filepath)
+                        )
+                        tasks.append(task)
+
+                    case MessageMediaType.WEB_PAGE:
+                        self.repository.add_link(message.web_page.url)
+
+                    case _:
+                        msg_text = message.text
+                        if msg_text:
+                            if 'https' in msg_text or 'http' in msg_text:
+                                logger.info(
+                                    f"!! ВНИМАНИЕ !! В текстовый документ была добавлена ссылка: '{message.text}'\n"
+                                    f"Необходимо вручную перейти по ней и скачать контент!")
+
+                                self.repository.add_link(message.text)
+
+            await asyncio.gather(*tasks)
+
+            logger.info("Сохраняем ссылки в файл...")
+            self.repository.save_links()
+            logger.info("Очищаем временные файлы...")
+            self.repository.clear_temps()
+
+            logger.info(f"{self.map}")
+            logger.info(f"Загрузка медиаконтента завершена за {time() - start:.2f} секунд.")
+
+    async def process_message(self, message: MessageMediaType, date: datetime, filename: str | None = None) -> str:
+        """ Обработка сообщения, возвращает путь для скачивания. """
+
+        map = {
+            MessageMediaType.VIDEO: "ролик",
+            MessageMediaType.DOCUMENT: "документ",
+            MessageMediaType.PHOTO: "фотография"
+        }
+
+        logger.info(f"Найден(а) {map.get(message)}: '{filename}', "
+                    f"дата отправки: {date.strftime('%d-%m-%Y %H:%M')}")
+
+        self.map[message] = self.map.get(message, 0) + 1
+
+        return self.repository.get_filepath(filename)
+
+    async def semaphore_wrapper(self, coro, *args, **kwargs):
+        async with self.semaphore:
+            return await coro(*args, **kwargs)
+
+    @staticmethod
+    async def _download_with_notification(message: Message, filepath: str):
+        """ Скачивание файла с уведомлением. """
+
+        for attempt in range(3):
+            try:
+                await message.download(filepath)
+                logger.info(f"Скачивание файла '{filepath.split("\\")[-1]}' завершено.")
+                await asyncio.sleep(1)
+
+                break
+            except PermissionError as e:
+                if attempt < 2:
+                    logger.warning(f"Ошибка доступа к файлу {filepath}, повтор через 1 секунду... ({e})")
+                    filepath = f"{filepath}.{uuid.uuid4().hex}.temp"
+
+                    await asyncio.sleep(1)
+                else:
+                    logger.error(f"Скачивание файла {filepath} завершилось ошибкой: {e}")
+
+                    raise
