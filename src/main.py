dcaf43a80580f67e443f3b9a91b2fa5afca5c5d0
Vladislav Bodrevskiy <anpathfinder@mail.ru>
2024-12-23 11:24:14 +0300

    add: main

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from pyrogram import Client
+
+from src.core.config import settings
+from src.services.telegram import PyrogramClient
+from src.services.repository import FileRepository
+
+
+def main():
+    app = Client(
+        name=settings.SESSION_NAME,
+        api_id=settings.API_ID,
+        api_hash=settings.API_HASH
+    )
+    repository = FileRepository()
+
+    client = PyrogramClient(app, repository)
+    client.run()
+
+
+if __name__ == '__main__':
+    main()
